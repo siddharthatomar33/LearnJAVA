80b5e7822562520b9aa76dc0a0f92ec2dfe425d7
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-03-24 12:29:52 +0530

    python program

---
+++ loop.py
@@ -0,0 +1,11 @@
+# #i=1
+j=int(input("enter the no."))
+# while(j>=0):
+#     print("* "*i)
+#     j-=1
+#     i+=1
+
+for i in range(j):
+    for k in range(j):
+        print("* ",end="")
+    print()
