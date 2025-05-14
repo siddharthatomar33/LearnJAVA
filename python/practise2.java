7fa2d5a2a37d4963f6bc6ac44719aa81539df6aa
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-05-14 16:35:19 +0530

    java program

---
+++ python/practise2.java
@@ -0,0 +1,28 @@
+class employee{
+    int id;
+    String name;
+    public void printdetails(){
+        System.out.println("my id is" +id);
+        System.out.println("my name is" +name);
+    }
+}
+public class practise2{
+    public static void main(String[] args){
+        System.out.println("this is our custom class");
+        employee harry =new employee(); //instantiated a new employee object
+        employee john =new employee();
+
+        //setting attribute
+        harry.id=12;
+        harry.name="codewith harry";
+        john.id=13;
+        john.name="john badmash";
+
+        //printing the atrributes  
+        harry.printdetails();
+        john.printdetails();
+
+        // System.out.println(harry.id);
+        // System.out.println(harry.name);
+    }
+}
