3c0b7525e8b2752cde45527e8404cccc8c98e613
Siddhartha Tomar <siddharthatomar33@gmail.com>
2025-05-15 08:49:01 +0530

    Add files via upload

---
+++ practise5.java
@@ -0,0 +1,30 @@
+class myemployee{
+private int id;
+private String name;
+
+public void setname(String n){
+    name=n;
+}
+public String getname(){
+    return name;
+}
+public void setid(int i){
+    id=i;
+}
+public int getid(){
+    return id;
+}
+
+}
+
+
+public class practise5{
+    public static void main(String[] args) {
+        myemployee harry=new myemployee();
+        
+        harry.setname("codewithharry");
+        System.out.println(harry.getname());
+        // harry.id=45;
+        // harry.name="codewithharry";
+    }
+}
